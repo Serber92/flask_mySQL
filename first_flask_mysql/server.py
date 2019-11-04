8d647b058bb8ab8868ee22b4817a5f98a14989c7
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-11-03 19:26:09 -0800

    added first_flask_mysql project

--- first_flask_mysql/server.py
+++ first_flask_mysql/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     # call the function, passing in the name of our db
@@ -9,8 +10,21 @@ def index():
     # call the query_db function, pass in the query as a string
     friends = mysql.query_db('SELECT * FROM friends;')
     print(friends)
-    return render_template("index.html")
+    return render_template("index.html", all_friends=friends)
 
+
+@app.route("/create_friend", methods=["POST"])
+def add_friend_to_db():
+
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(fn)s, %(ln)s, %(occup)s, NOW(), NOW());"
+    data = {
+        "fn": request.form["fname"],
+        "ln": request.form['lname'],
+        "occup": request.form["occupation"]
+    }
+    db = connectToMySQL('first_flask')
+    db.query_db(query, data)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
